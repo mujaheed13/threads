6e29ef9d5bc15235deedaf427e8a5451a22749ed
mujaheed13 <mujaheedsami13@gmail.com>
2024-01-18 11:56:41 +0530

    DB connection & User model

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,1 +1,7 @@
 "use server";
+
+import { connectToDB } from "../mongoose";
+
+export async function upadateUser(): Promise<void> {
+    connectToDB()
+}
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -8,5 +8,12 @@ export const connectToDB = async () => {
   if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
   if (!isConnected) return console.log("Already connected to MongoDB");
   try {
-  } catch (error) {}
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    isConnected = true;
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
 };
